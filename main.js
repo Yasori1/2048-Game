3f475b47febc7946c75d1c57a0c54450a3c396c3
Eren <yasar451278@gmail.com>
2024-07-13 16:25:10 +0300

    up

--- main.js
+++ main.js
@@ -138,6 +138,102 @@ Game.prototype.TileMerge = function () {
       newScore += curruntValue
     }
   })
-this.score = newScore;
-$('[data-js="score"]').html(this.score.toString ());
+  this.score = newScore
+  $('[data-js="score"]').html(this.score.toString())
+}
+
+Game.prototype.moveAnimations = function () {
+  var self = this
+  var promiseArray = []
+
+  if (this.moveInProgress) {
+    return false
+  }
+
+  this.moveInProgress = true
+  gameBoard.forEach(function (val, index, array) {
+    val.tilesArray.forEach(function (val, index, array) {
+      promiseArray.push(val.animetePosition())
+    })
+  })
+  $.when.apply($, promiseArray).done(function () {
+    self.moveInProgress = false
+    self.TileMerge()
+    self.initTile()
+  })
+}
+
+Game.prototype.move = function (getDirection) {
+    var gameBoard;
+    var direction = getDirection.toLowerCase()
+    var hasAnyTileMoved = false;
+    if (this.moveInProgress){
+        return false
+    }
+    if (direction ==="up"){
+        gameBoard = _.orderBy(this.boardFlatten(),"y","asc");
+    }else if (direction ==="right"){
+        gameBoard = _.orderBy(this.boardFlatten(),"x","desc");
+    }else if (direction ==="down"){
+        gameBoard = _.orderBy(this.boardFlatten(),"y","desc");
+    }else if (direction ==="left"){
+        gameBoard = _.orderBy(this.boardFlatten(),"y","asc");
+    }
+
+    gameBoard.forEach(function (val, index, array) {
+        val.tilesArray.length
+          ? val.tilesArray.forEach(function (val) {
+              if (val.move(direction, true)) {
+                hasAnyTileMoved = true
+                val.move(direction)
+              }
+            })
+          : false
+      })
+      hasAnyTileMoved ? this.moveAnimations(gameBoard) : false
+}
+
+function Tile(x,y,game){
+    this.game = game
+    this.el
+    this.x=x
+    this.y=y
+    this.valueProp = 2
+    Object.defineProperties(this, {
+        value: {
+          get: function () {
+            return this.valueProp
+          },
+          set: function (val) {
+            this.valueProp = val
+            this.el
+              .find('.tile_number')
+              .html(this.valueProp)
+              .attr('data-value', val)
+          },
+        },
+      })
+      // can move flag
+      this.canMove = false
+      // initialize
+      this.initialize()
+}
+
+Tile.prototype.initialize = function () {
+    var getTile = $.parseHTML($('#template_tile').html())
+    this.el = $(getTile)
+    this.el.find('.tile_number').html(this.valueProp).attr('data-value',2)
+    this.setPosition(this.x, this.y)
+    this.animetePosition(true)
+    this.el.appendTo('.tile-container')
+}
+
+Tile.prototype.setPosition = function (getX, getY) {
+    this.x=getX
+    this.y=getY
+    this.game.board[getX][getY].tilesArray.push(this)
+}
+
+Tile.prototype.removeOldPosition = function (getX, getY) {
+    this.game.board[getX][getY].tilesArray.pop()
 }
