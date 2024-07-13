c1b42177a2bb1c92e98e0e0ee65b8cbe7ae21330
Eren <yasar451278@gmail.com>
2024-07-13 16:49:23 +0300

    up

--- main.js
+++ main.js
@@ -164,76 +164,167 @@ Game.prototype.moveAnimations = function () {
 }
 
 Game.prototype.move = function (getDirection) {
-    var gameBoard;
-    var direction = getDirection.toLowerCase()
-    var hasAnyTileMoved = false;
-    if (this.moveInProgress){
-        return false
-    }
-    if (direction ==="up"){
-        gameBoard = _.orderBy(this.boardFlatten(),"y","asc");
-    }else if (direction ==="right"){
-        gameBoard = _.orderBy(this.boardFlatten(),"x","desc");
-    }else if (direction ==="down"){
-        gameBoard = _.orderBy(this.boardFlatten(),"y","desc");
-    }else if (direction ==="left"){
-        gameBoard = _.orderBy(this.boardFlatten(),"y","asc");
-    }
+  var gameBoard
+  var direction = getDirection.toLowerCase()
+  var hasAnyTileMoved = false
+  if (this.moveInProgress) {
+    return false
+  }
+  if (direction === 'up') {
+    gameBoard = _.orderBy(this.boardFlatten(), 'y', 'asc')
+  } else if (direction === 'right') {
+    gameBoard = _.orderBy(this.boardFlatten(), 'x', 'desc')
+  } else if (direction === 'down') {
+    gameBoard = _.orderBy(this.boardFlatten(), 'y', 'desc')
+  } else if (direction === 'left') {
+    gameBoard = _.orderBy(this.boardFlatten(), 'y', 'asc')
+  }
 
-    gameBoard.forEach(function (val, index, array) {
-        val.tilesArray.length
-          ? val.tilesArray.forEach(function (val) {
-              if (val.move(direction, true)) {
-                hasAnyTileMoved = true
-                val.move(direction)
-              }
-            })
-          : false
-      })
-      hasAnyTileMoved ? this.moveAnimations(gameBoard) : false
+  gameBoard.forEach(function (val, index, array) {
+    val.tilesArray.length
+      ? val.tilesArray.forEach(function (val) {
+          if (val.move(direction, true)) {
+            hasAnyTileMoved = true
+            val.move(direction)
+          }
+        })
+      : false
+  })
+  hasAnyTileMoved ? this.moveAnimations(gameBoard) : false
 }
 
-function Tile(x,y,game){
-    this.game = game
-    this.el
-    this.x=x
-    this.y=y
-    this.valueProp = 2
-    Object.defineProperties(this, {
-        value: {
-          get: function () {
-            return this.valueProp
-          },
-          set: function (val) {
-            this.valueProp = val
-            this.el
-              .find('.tile_number')
-              .html(this.valueProp)
-              .attr('data-value', val)
-          },
-        },
-      })
-      // can move flag
-      this.canMove = false
-      // initialize
-      this.initialize()
+function Tile(x, y, game) {
+  this.game = game
+  this.el
+  this.x = x
+  this.y = y
+  this.valueProp = 2
+  Object.defineProperties(this, {
+    value: {
+      get: function () {
+        return this.valueProp
+      },
+      set: function (val) {
+        this.valueProp = val
+        this.el
+          .find('.tile_number')
+          .html(this.valueProp)
+          .attr('data-value', val)
+      },
+    },
+  })
+  // can move flag
+  this.canMove = false
+  // initialize
+  this.initialize()
 }
 
 Tile.prototype.initialize = function () {
-    var getTile = $.parseHTML($('#template_tile').html())
-    this.el = $(getTile)
-    this.el.find('.tile_number').html(this.valueProp).attr('data-value',2)
-    this.setPosition(this.x, this.y)
-    this.animetePosition(true)
-    this.el.appendTo('.tile-container')
+  var getTile = $.parseHTML($('#template_tile').html())
+  this.el = $(getTile)
+  this.el.find('.tile_number').html(this.valueProp).attr('data-value', 2)
+  this.setPosition(this.x, this.y)
+  this.animetePosition(true)
+  this.el.appendTo('.tile-container')
 }
 
 Tile.prototype.setPosition = function (getX, getY) {
-    this.x=getX
-    this.y=getY
-    this.game.board[getX][getY].tilesArray.push(this)
+  this.x = getX
+  this.y = getY
+  this.game.board[getX][getY].tilesArray.push(this)
 }
 
 Tile.prototype.removeOldPosition = function (getX, getY) {
-    this.game.board[getX][getY].tilesArray.pop()
+  this.game.board[getX][getY].tilesArray.pop()
+}
+
+Tile.prototype.animetePosition = function (initializeFlag) {
+  var self = this
+  var fromleft = this.x * (100 / this.game.rows)
+  var fromtop = this.y * (100 / this.game.colums)
+
+  var animationDuration = 175
+  var getpromise = $.Defferred()
+
+  if (initializeFlag) {
+    this.el.addClass('initialize')
+  } else {
+    this.el.removeClass('initialize')
+  }
+  function resolvePromise() {
+    getpromise.resolve()
+    self.el.removeClass('animate')
+    self.el.removeClass('initialize')
+  }
+  function setPosition() {
+    self.el.addClass('animate')
+    self.el.attr({
+      'data-x': fromleft,
+      'data-y': fromtop,
+    })
+  }
+  if (initializeFlag) {
+    setPosition()
+    window.setTimeout(resolvePromise, animationDuration + 50)
+  } else {
+    setPosition()
+    window.setTimeout(resolvePromise, animationDuration)
+  }
+  return getpromise
+}
+
+Tile.prototype.moveCheck = function () {
+  if (
+    this.move('up', true) ||
+    this.move('right', true) ||
+    this.move('down', true) ||
+    this.move('left', true)
+  ) {
+    this.canMove = true
+    return true
+  } else {
+    this.canMove = false
+    return false
+  }
+}
+
+Tile.prototype.move = function (getDirection, initializeFlag) {
+  var checkFlag = checkFlag ? true : false
+  var direction = getDirection.toLowerCase()
+  var getX = this.x
+  var getY = this.y
+
+  var getNext
+  var isNextMatch
+  var isNextEmpty
+  var nextPositionArray = []
+
+  if (direction === 'up') {
+    getNext = this.y > 0 ? this.game.board[this.x][this.y - 1] : false
+    nextPositionArray.push(this.x, this.y - 1)
+  } else if (direction === 'right') {
+    getNext = this.x < 3 ? this.game.board[this.x + 1][this.y] : false
+    nextPositionArray.push(this.x + 1, this.y)
+  } else if (direction === 'down') {
+    getNext = this.y < 3 ? this.game.board[this.x][this.y + 1] : false
+    nextPositionArray.push(this.x, this.y + 1)
+  } else if (direction === 'left') {
+    getNext = this.x > 0 ? this.game.board[this.x - 1][this.y] : false
+    nextPositionArray.push(this.x - 1, this.y)
+  }
+  isNextMatch =
+    getNext &&
+    getNext.tileArray.length === 1 &&
+    getNext.tilesArray[0].valueProp === this.valueProp
+  isNextEmpty = getNext && getNext.tileArray.length === 0
+
+  if (checkFlag) {
+    return isNextEmpty || isNextMatch ? true : false
+  } else if (isNextEmpty || isNextMatch) {
+    this.setPosition(nextPositionArray[0], nextPositionArray[1])
+    this.removeOldPosition(getX, getY)
+    if (isNextMatch) {
+      this.move(direction)
+    }
+  }
 }
